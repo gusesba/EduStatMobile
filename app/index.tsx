66c17fe61e87361bfc5eaff62d0440f913b84d46
Gustavo Esmanhotto Bareta <gustavoesmanhotto@hotmail.com>
2024-11-05 11:01:59 -0300

    paper lib config

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet} from 'react-native';
+import {Button} from 'react-native-paper';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Edustat</Text>
+      <View style={styles.buttons}>
+        <Button mode='elevated'>Login</Button>
+        <Button mode='outlined'>Home</Button>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    paddingTop: 100
+
+  },
+  text: {
+    fontSize: 50,
+    color: '#fff',
+  },
+  buttons: {
+    flex: 1,
+    justifyContent:'center',
+    gap: 10
+  }
+  
+});
