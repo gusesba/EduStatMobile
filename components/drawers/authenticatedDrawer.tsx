a5fd4f46ef8d03bd32f314f7386d160320775946
Gustavo Esmanhotto Bareta <gustavo.bareta@mps.interno>
2025-01-27 13:16:16 -0300

    help video

--- components/drawers/authenticatedDrawer.tsx
+++ components/drawers/authenticatedDrawer.tsx
@@ -10,7 +10,83 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import Teams from "@/app/teams";
 import Chatbot from "@/app/chatbot";
 import Main from "@/app/main";
+import { TouchableOpacity } from "react-native";
+import { View, Text } from "react-native";
+import { ResizeMode, Video } from "expo-av"; // Importando o player de vídeo
+import { useNavigation } from "expo-router";
+import { NativeStackNavigationProp } from "react-native-screens/lib/typescript/native-stack/types";
+import { useEffect, useRef } from "react";
+import { useIsFocused } from "@react-navigation/native";
 const Drawer = createDrawerNavigator();
+
+type RootStackParamList = {
+  main: undefined;
+  register: undefined;
+  help: { videoSource: any }; // Definindo que a tela 'help' espera o parâmetro 'videoSource'
+  potentiostat: undefined;
+  experiments: undefined;
+  educhat: undefined;
+  simulation: undefined;
+  settings: undefined;
+  teams: undefined;
+  login: undefined;
+};
+
+const videoSources = {
+  potentiostat: require("@/assets/videos/potentiostat_help.mp4"),
+
+  // Adicione outros vídeos conforme necessário
+};
+
+function HelpVideoScreen({ route }: { route: any }) {
+  const { videoSource } = route.params; // Recebe o caminho do vídeo via parâmetros
+  const isFocused = useIsFocused();
+
+  // Cria uma referência para o componente de vídeo
+  const videoRef = useRef(null);
+
+  // Função para reiniciar o vídeo
+  const handleReplay = () => {
+    // @ts-ignore
+    videoRef.current?.replayAsync(); // Reinicia o vídeo
+  };
+
+  useEffect(() => {
+    if (isFocused) {
+      handleReplay();
+    }
+  }, [isFocused]);
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Video
+        ref={videoRef} // Atribui a referência ao componente Video
+        source={videoSource} // Usa o caminho do vídeo passado como parâmetro
+        style={{ width: "100%", height: "100%" }}
+        useNativeControls={true} // Mostra controles do player
+        resizeMode={ResizeMode.CONTAIN} // Ajusta o vídeo para caber na tela
+        shouldPlay={true}
+      />
+    </View>
+  );
+}
+
+function HelpButton({ videoKey }: { videoKey: keyof typeof videoSources }) {
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+  return (
+    <TouchableOpacity
+      onPress={() =>
+        navigation.navigate("help", {
+          videoSource: videoSources[videoKey], // Usando o mapa para obter o caminho do vídeo
+        })
+      }
+      style={{ marginRight: 15 }}
+    >
+      <Ionicons name="help-circle-outline" size={24} color="black" />
+    </TouchableOpacity>
+  );
+}
 
 export default function AuthenticatedDrawer() {
   return (
@@ -26,10 +102,20 @@ export default function AuthenticatedDrawer() {
         options={{ headerShown: false, drawerItemStyle: { display: "none" } }}
       />
       <Drawer.Screen
+        name="help"
+        component={HelpVideoScreen} // Componente da tela de ajuda
+        initialParams={{ videoSource: require("@/assets/videos/help.mp4") }}
+        options={{
+          title: "Help Video",
+          drawerItemStyle: { display: "none" },
+        }}
+      />
+      <Drawer.Screen
         name="potentiostat"
         component={Potentiostat}
         options={{
           title: "Potentiostat",
+          headerRight: () => <HelpButton videoKey="potentiostat" />,
           drawerIcon: ({ color, size }) => (
             <Ionicons name="flask-outline" color={color} size={size} />
           ),
